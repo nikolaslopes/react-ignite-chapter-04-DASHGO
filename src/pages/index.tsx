4fffb2b23bf74d3940dcdc8f918ffe3af2676ac7
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-06-22 15:54:00 -0400

    :sparkles: feat: home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,77 @@
+import {
+  Button,
+  Flex,
+  Input,
+  Stack,
+  FormLabel,
+  FormControl,
+  FormHelperText,
+} from '@chakra-ui/react'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
     <>
-      <h1>teste</h1>
-      <header>
-        <h2>adsolkfnds</h2>
-      </header>
-      <p>configurate chackra</p>
+      <Flex
+        width={'100vw'}
+        height={'100vh'}
+        alignItems={'center'}
+        justifyContent={'center'}
+        border={'1px solid red'}
+      >
+        <Flex
+          as={'form'}
+          width={'100%'}
+          maxWidth={'360px'}
+          backgroundColor={'gray.800'}
+          padding={'8'}
+          borderRadius={'8px'}
+          flexDirection={'column'}
+        >
+          <Stack spacing={'4'}>
+            <FormControl>
+              <FormLabel htmlFor="email">E-mail</FormLabel>
+              <Input
+                id="email"
+                name="email"
+                type="email"
+                variant={'filled'}
+                focusBorderColor={'pink.500'}
+                backgroundColor={'gray.900'}
+                _hover={{
+                  backgroundColor: 'gray.900',
+                }}
+                size="lg"
+              />
+            </FormControl>
+
+            <FormControl>
+              <FormLabel htmlFor="password">Password</FormLabel>
+              <Input
+                id="password"
+                name="password"
+                type="password"
+                variant={'filled'}
+                focusBorderColor={'pink.500'}
+                backgroundColor={'gray.900'}
+                _hover={{
+                  backgroundColor: 'gray.900',
+                }}
+                size="lg"
+              />
+            </FormControl>
+          </Stack>
+
+          <Button
+            type="submit"
+            marginTop={'6'}
+            colorScheme={'pink'}
+            size={'lg'}
+          >
+            Sign In
+          </Button>
+        </Flex>
+      </Flex>
     </>
   )
 }
