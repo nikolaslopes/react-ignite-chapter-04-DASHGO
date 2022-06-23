707527f37deff20b18ef014eb23fea473bf839cb
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-06-23 11:39:37 -0400

    :construction: dashboard graphics

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,11 +1,11 @@
-import { Flex } from '@chakra-ui/react'
+import { Box, Flex, SimpleGrid, Text } from '@chakra-ui/react'
 import { NextPage } from 'next'
 import { Header } from '../components/Header'
 import { Sidebar } from '../components/Sidebar'
 
 const Dashboard: NextPage = () => {
   return (
-    <Flex direction={'column'} height={'100vh'} border={'1px solid blue'}>
+    <Flex direction={'column'} height={'100vh'}>
       <Header />
 
       <Flex
@@ -16,6 +16,20 @@ const Dashboard: NextPage = () => {
         paddingX={'6'}
       >
         <Sidebar />
+
+        <SimpleGrid flex={'1'} gap={'4'} minChildWidth={'320px'}>
+          <Box padding={'8'} backgroundColor={'gray.800'} borderRadius={'8'}>
+            <Text fontSize={'lg'} marginBottom={'4'}>
+              afs
+            </Text>
+          </Box>
+
+          <Box padding={'8'} backgroundColor={'gray.800'} borderRadius={'8'}>
+            <Text fontSize={'lg'} marginBottom={'4'}>
+              afs
+            </Text>
+          </Box>
+        </SimpleGrid>
       </Flex>
     </Flex>
   )
