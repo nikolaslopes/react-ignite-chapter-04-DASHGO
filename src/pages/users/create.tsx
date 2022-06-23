77bc10423dfeb60050b51227b4c84fbcd348a1b6
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-06-23 17:40:50 -0400

    :sparkles: feat: create user

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,4 +1,13 @@
-import { Box, Divider, Flex, Heading, Stack } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  HStack,
+  SimpleGrid,
+  Stack,
+} from '@chakra-ui/react'
 import { Input } from '../../components/Form/Input'
 import { Header } from '../../components/Header'
 import { Sidebar } from '../../components/Sidebar'
@@ -29,10 +38,28 @@ export default function UserCreate() {
 
           <Divider marginY={'6'} borderColor={'gray.700'} />
 
-          <Stack>
-            <Input name="name" label="Full name" />
-            <Input name="email" label="E-mail" />
+          <Stack spacing={'8'}>
+            <SimpleGrid minChildWidth={'240px'} spacing={'8'}>
+              <Input name="name" label="Full name" />
+              <Input name="email" label="E-mail" />
+            </SimpleGrid>
+
+            <SimpleGrid minChildWidth={'240px'} spacing={'8'}>
+              <Input name="password" label="Password" />
+              <Input name="password_confirmation" label="Confirm passowrd" />
+            </SimpleGrid>
           </Stack>
+
+          <Flex marginTop={'8'} justifyContent={'flex-end'}>
+            <HStack spacing={'4'}>
+              <Button width="20" colorScheme={'whiteAlpha'}>
+                Cancel
+              </Button>
+              <Button width="20" colorScheme={'pink'}>
+                Confirm
+              </Button>
+            </HStack>
+          </Flex>
         </Box>
       </Flex>
     </Box>
