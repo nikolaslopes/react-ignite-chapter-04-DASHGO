964ea0d437d0f2c15a3afe3b16cde2fc15784b66
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-06-23 12:30:06 -0400

    :construction: dashboard graphics

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,63 @@
-import { Box, Flex, SimpleGrid, Text } from '@chakra-ui/react'
+import { Box, Flex, SimpleGrid, Text, theme } from '@chakra-ui/react'
 import { NextPage } from 'next'
-import { Header } from '../components/Header'
+import dynamic from 'next/dynamic'
 import { Sidebar } from '../components/Sidebar'
+import { Header } from '../components/Header'
+import { ApexOptions } from 'apexcharts'
+
+const Chart = dynamic(() => import('react-apexcharts'), {
+  ssr: false,
+})
+
+const options: ApexOptions = {
+  chart: {
+    toolbar: {
+      show: false,
+    },
+    zoom: {
+      enabled: false,
+    },
+    foreColor: theme.colors.gray[500],
+  },
+  grid: {
+    show: false,
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  tooltip: {
+    enabled: false,
+  },
+  xaxis: {
+    type: 'datetime',
+    axisBorder: {
+      color: theme.colors.gray[600],
+    },
+    axisTicks: {
+      color: theme.colors.gray[600],
+    },
+    categories: [
+      '2022-03-18T00:00:00.000Z',
+      '2022-03-19T00:00:00.000Z',
+      '2022-03-20T00:00:00.000Z',
+      '2022-03-21T00:00:00.000Z',
+      '2022-03-22T00:00:00.000Z',
+      '2022-03-23T00:00:00.000Z',
+      '2022-03-24T00:00:00.000Z',
+    ],
+  },
+  fill: {
+    opacity: 0.3,
+    type: 'gradient',
+    gradient: {
+      shade: 'dark',
+      opacityFrom: 0.7,
+      opacityTo: 0.3,
+    },
+  },
+}
+
+const series = [{ name: 'series1', data: [31, 120, 10, 28, 18, 109, 100] }]
 
 const Dashboard: NextPage = () => {
   return (
@@ -18,16 +74,23 @@ const Dashboard: NextPage = () => {
         <Sidebar />
 
         <SimpleGrid flex={'1'} gap={'4'} minChildWidth={'320px'}>
-          <Box padding={'8'} backgroundColor={'gray.800'} borderRadius={'8'}>
+          <Box
+            padding={'8'}
+            backgroundColor={'gray.800'}
+            borderRadius={'8'}
+            paddingBottom={'4'}
+          >
             <Text fontSize={'lg'} marginBottom={'4'}>
-              afs
+              Subscribers of the week
             </Text>
+            <Chart type="area" height={160} options={options} series={series} />
           </Box>
 
           <Box padding={'8'} backgroundColor={'gray.800'} borderRadius={'8'}>
             <Text fontSize={'lg'} marginBottom={'4'}>
-              afs
+              Open rate
             </Text>
+            <Chart type="area" height={160} options={options} series={series} />
           </Box>
         </SimpleGrid>
       </Flex>
