b725677745c4094bfad016383eae2c9a5393b41d
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-06-24 19:03:22 -0400

    :recycle: refactor: form responsivity

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -30,7 +30,7 @@ export default function UserCreate() {
           flex={'1'}
           borderRadius={'8'}
           backgroundColor={'gray.800'}
-          padding={'8'}
+          padding={['6', '8']}
         >
           <Heading size={'lg'} fontWeight={'normal'}>
             Create User
@@ -39,12 +39,12 @@ export default function UserCreate() {
           <Divider marginY={'6'} borderColor={'gray.700'} />
 
           <Stack spacing={'8'}>
-            <SimpleGrid minChildWidth={'240px'} spacing={'8'}>
+            <SimpleGrid minChildWidth={'240px'} spacing={['6', '8']}>
               <Input name="name" label="Full name" />
               <Input name="email" label="E-mail" />
             </SimpleGrid>
 
-            <SimpleGrid minChildWidth={'240px'} spacing={'8'}>
+            <SimpleGrid minChildWidth={'240px'} spacing={['6', '8']}>
               <Input name="password" label="Password" />
               <Input name="password_confirmation" label="Confirm passowrd" />
             </SimpleGrid>
