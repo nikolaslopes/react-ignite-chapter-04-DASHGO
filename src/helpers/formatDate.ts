3db7640edb173887350560ebf1c6b578291a1e7b
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-06-30 01:19:30 -0400

    :fire: created date helper

--- src/helpers/formatDate.ts
+++ src/helpers/formatDate.ts
@@ -2,7 +2,7 @@ import { format } from 'date-fns'
 import enUS from 'date-fns/locale/en-US'
 
 export function FormatDate(date: string) {
-  return format(new Date(date), 'M/dd/yyyy', {
+  return format(new Date(date), 'LLLL dd, yyyy', {
     locale: enUS,
   })
 }
