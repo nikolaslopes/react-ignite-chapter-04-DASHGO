ac70a6bc34fa3c9b52003981a26813b20b886484
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-06-29 23:41:43 -0400

    :wrench: mirage server routes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 import { SidebarDrawerProvider } from '../context/SidebarDrawerContext'
-import { makeServer } from '../services/mirage'
 import { theme } from '../styles/theme'
+import { makeServer } from '../services/mirage'
 
 if (process.env.NODE_ENV === 'development') {
   makeServer()
--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -13,8 +13,14 @@ export function makeServer() {
     },
 
     routes() {
+      this.namespace = 'api' // * Sets the namespace to use the routes
+      this.timing = 750
+
       this.get('/users')
       this.post('/users')
+
+      this.namespace = '' // * Reset namespace to don't cause trouble with Next API routes
+      this.passthrough() // * All API calls to '/api' address will be passed through Mirage, but if the call would not a mirage route, it will be passed on from your original route
     },
   })
 
