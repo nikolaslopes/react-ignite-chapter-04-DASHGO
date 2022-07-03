174ffb564cdc0c25d69226f4e4b6e81fe6134321
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-07-03 01:07:53 -0400

    :wrench: config user api headers

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model } from 'miragejs'
+import { createServer, Factory, Model, Response } from 'miragejs'
 import { faker } from '@faker-js/faker'
 import { IUser } from './types'
 
@@ -23,14 +23,28 @@ export function makeServer() {
     },
 
     seeds(server) {
-      server.createList('user', 10)
+      server.createList('user', 200)
     },
 
     routes() {
       this.namespace = 'api' // * Sets the namespace to use the routes
       this.timing = 750
 
-      this.get('/users')
+      this.get('/users', function (schema, request) {
+        const { page = 1, per_page = 10 } = request.queryParams
+
+        const total = schema.all('user').length
+
+        const pageStart = (Number(page) - 1) * Number(per_page)
+        const pageEnd = pageStart + Number(per_page)
+
+        const users = this.serialize(schema.all('user')).users.slice(
+          pageStart,
+          pageEnd
+        )
+
+        return new Response(200, { 'x-total-count': String(total) }, { users })
+      })
       this.post('/users')
 
       this.namespace = '' // * Reset namespace to don't cause trouble with Next API routes
